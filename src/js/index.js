8a666aefbb9dab9f85f603f85781b31c8f55dc88
Jake Goodman <jakeg527@gmail.com>
2018-09-29 11:52:03 -0400

    Change up tailwindcss settings

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import slick from 'slick-carousel';
+import lazysizes from 'lazysizes';
 
 // Create global functions for onclick with "window"
 window.toggle = function toggle(elem) {
@@ -11,9 +12,4 @@ window.toggleMenu = function toggleMenu() {
     var topNav = document.getElementById("topNav");
     console.log("test");
     topNav.classList.toggle('hidden');
-    topNav.classList.toggle('w-full');
-    topNav.classList.toggle('text-center');
-    var children = topNav.getElementsByTagName("*");
-    children.classList.toggle('w-full');
-    children.classList.toggle('p-3');
 }
